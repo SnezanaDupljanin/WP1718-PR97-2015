9506b5f7efd3a4707ea085bb7c1c2d57056b6533
ASUS <ASUS@DESKTOP-SL1QFU2>
2018-07-03 17:20:17 +0200

    Dodavanje projekta

---
+++ WebAPI/WebAPI/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        public override void Init()
+        {
+            this.PostAuthenticateRequest += MyPostAuthenticateRequest;
+            base.Init();
+        }
+
+
+        void MyPostAuthenticateRequest(object sender, EventArgs e)
+        {
+
+            System.Web.HttpContext.Current.SetSessionStateBehavior(SessionStateBehavior.Required);
+
+        }
+    }
+}
