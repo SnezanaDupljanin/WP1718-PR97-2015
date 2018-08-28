d3256803cbe69f05c325bb51d38ed832fdc9d66e
ASUS <ASUS@DESKTOP-SL1QFU2>
2018-08-28 23:08:04 +0200

    Dodate funkcije za Dispecera

--- WebAPI/WebAPI/Controllers/DispecerController.cs
+++ WebAPI/WebAPI/Controllers/DispecerController.cs
@@ -24,5 +24,77 @@ namespace WebAPI.Controllers
             else
                 return "";
         }
+
+        [MyAuthorization(Roles = "Administrator")]
+        [HttpGet]
+        [Route("api/Dispecer/VratiSveVoznje")]
+        public List<Voznja> VratiSveVoznje()
+        {
+            List<Voznja> ret = new List<Voznja>();
+            foreach (var item in Korisnici.ListaMusterija)
+            {
+                ret.AddRange(item.Voznje);
+            }
+            foreach (var item in Korisnici.ListaDispecera)
+            {
+                foreach (var item2 in item.Voznje)
+                {
+                    if (!ret.Contains(item2))
+                    {
+                        ret.Add(item2);
+                    }
+                }
+            }
+            return ret;
+        }
+
+        [MyAuthorization(Roles = "Administrator")]
+        [HttpGet]
+        [Route("api/Dispecer/VratiSveKorisnike")]
+        public List<string> VratiSveKorisnike()
+        {
+            List<string> ret = new List<string>();
+            foreach (var item in Korisnici.ListaMusterija)
+            {
+                ret.Add(item.KorisnickoIme + "-musterija-" + item.Banovan.ToString());
+            }
+            foreach (var item in Korisnici.ListaVozaca)
+            {
+                ret.Add(item.KorisnickoIme + "-vozac-" + item.Banovan.ToString());
+            }
+            return ret;
+        }
+
+        [MyAuthorization(Roles = "Administrator")]
+        [HttpGet]
+        [Route("api/Dispecer/Blokiranje/")]
+        public List<string> Blokiranje(string korIme)
+        {
+            if (Korisnici.ListaMusterija.FirstOrDefault(m => m.KorisnickoIme == korIme) != null)
+            {
+                Korisnici.ListaMusterija.FirstOrDefault(m => m.KorisnickoIme == korIme).Banovan = !Korisnici.ListaMusterija.FirstOrDefault(m => m.KorisnickoIme == korIme).Banovan;
+                if (File.Exists(Korisnici.PutanjaMusterije))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Musterija>));
+                    using (StreamWriter writer = new StreamWriter(Korisnici.PutanjaMusterije, false))
+                    {
+                        xmlSerializer.Serialize(writer, Korisnici.ListaMusterija);
+                    }
+                }
+            }
+            else if (Korisnici.ListaVozaca.FirstOrDefault(m => m.KorisnickoIme == korIme) != null)
+            {
+                Korisnici.ListaVozaca.FirstOrDefault(m => m.KorisnickoIme == korIme).Banovan = !Korisnici.ListaVozaca.FirstOrDefault(m => m.KorisnickoIme == korIme).Banovan;
+                if (File.Exists(Korisnici.PutanjaVozaci))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Vozac>));
+                    using (StreamWriter writer = new StreamWriter(Korisnici.PutanjaVozaci, false))
+                    {
+                        xmlSerializer.Serialize(writer, Korisnici.ListaVozaca);
+                    }
+                }
+            }
+            return VratiSveKorisnike();
+        }
     }
 }
