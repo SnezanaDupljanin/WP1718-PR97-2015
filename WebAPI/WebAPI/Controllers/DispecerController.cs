0731b29fc18d5a1e70db113b259c386df5a5dbd2
ASUS <ASUS@DESKTOP-SL1QFU2>
2018-08-28 23:52:02 +0200

    Get/Post meteode, model Voznja

--- WebAPI/WebAPI/Controllers/DispecerController.cs
+++ WebAPI/WebAPI/Controllers/DispecerController.cs
@@ -96,5 +96,81 @@ namespace WebAPI.Controllers
             }
             return VratiSveKorisnike();
         }
+        [MyAuthorization(Roles = "Administrator")]
+        // GET: api/Dispecer/5
+        public Dispecer Get()
+        {
+            Dispecer k = (Dispecer)System.Web.HttpContext.Current.Session["mojaSesija"];
+
+            return k;
+        }
+        [MyAuthorization(Roles = "Administrator")]
+        public HttpResponseMessage Get(string x, string y, string tip, string ulica, string broj, string posta, string grad, string korIme, string vozac)
+        {
+            HttpResponseMessage ret = new HttpResponseMessage();
+            ret.StatusCode = HttpStatusCode.OK;
+            string xxx = ulica;
+            string usernameVozaca = vozac;
+            Adresa adresa = new Adresa() { Ulica = ulica, Broj = broj, NaseljenoMjesto = grad, PozivniBrojMjesta = posta };
+            Lokacija lokacija = new Lokacija() { Adresa = adresa, KoordinataX = x, KoordinataY = y };
+            Voznja voznja = new Voznja() { DatumIVrijemePorudzbe = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Unspecified), Musterija = "nepoznato", Vozac = vozac, Dispecer = korIme, StatusVoznje = StatusiVoznje.Formirana, Lokacija = lokacija, Komentar = new Komentar() { Opis = "", Ocjena = Ocjene.Neocijenjeno }, TipAutomobila = (TipoviAutomobila)(int.Parse(tip)) };
+            Dispecer disp = Korisnici.ListaDispecera.FirstOrDefault(m => m.KorisnickoIme == korIme);
+            
+            Korisnici.ListaDispecera.FirstOrDefault(m => m.KorisnickoIme == korIme).Voznje.Add(voznja);
+            Korisnici.ListaVozaca.FirstOrDefault(v => v.KorisnickoIme == vozac).Voznje.Add(voznja);
+            Korisnici.ListaVozaca.FirstOrDefault(v => v.KorisnickoIme == vozac).Zauzet = true;
+            if (File.Exists(Korisnici.PutanjaVozaci))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Vozac>));
+                using (StreamWriter writer = new StreamWriter(Korisnici.PutanjaVozaci, false))
+                {
+                    xmlSerializer.Serialize(writer, Korisnici.ListaVozaca);
+                }
+            }
+            if (File.Exists(Korisnici.PutanjaDispeceri))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Dispecer>));
+                using (StreamWriter writer = new StreamWriter(Korisnici.PutanjaDispeceri, false))
+                {
+                    xmlSerializer.Serialize(writer, Korisnici.ListaDispecera);
+                }
+            }
+            System.Web.HttpContext.Current.Session["mojaSesija"] = Korisnici.ListaDispecera.FirstOrDefault(m => m.KorisnickoIme == korIme);
+            return ret;
+        }
+
+        [MyAuthorization(Roles = "Administrator")]
+        // POST: api/Dispecer
+        public HttpResponseMessage Post([FromBody]Dispecer dispecer)
+        {
+            string ime = dispecer.Ime;
+            HttpResponseMessage mess = new HttpResponseMessage();
+            //mora postojati ako je izmjena podataka
+            if (Korisnici.ListaDispecera.FirstOrDefault(d => dispecer.KorisnickoIme == d.KorisnickoIme) != null)
+            {
+                int ind = Korisnici.ListaDispecera.IndexOf(Korisnici.ListaDispecera.FirstOrDefault(d => dispecer.KorisnickoIme == d.KorisnickoIme));
+                //ne postoji korisnicko ime do sad
+                dispecer.Uloga = Uloge.Dispecer;
+                dispecer.Voznje = Korisnici.ListaDispecera[ind].Voznje;
+                Korisnici.ListaDispecera[ind] = dispecer;
+                if (File.Exists(Korisnici.PutanjaDispeceri))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Dispecer>));
+                    using (StreamWriter writer = new StreamWriter(Korisnici.PutanjaDispeceri, false))
+                    {
+                        xmlSerializer.Serialize(writer, Korisnici.ListaDispecera);
+                    }
+                }
+                System.Web.HttpContext.Current.Session["mojaSesija"] = dispecer;
+                mess.StatusCode = HttpStatusCode.OK;
+                return mess;
+            }
+            else
+            {
+                // postoji korisnicko ime vec
+                mess.StatusCode = HttpStatusCode.NotAcceptable;
+                return mess;
+            }
+        }
     }
 }
--- WebAPI/WebAPI/Models/Voznja.cs
+++ WebAPI/WebAPI/Models/Voznja.cs
@@ -7,5 +7,38 @@ namespace WebAPI.Models
 {
     public class Voznja
     {
+        private static int brojac = 0;
+        public Voznja()
+        {
+            Lokacija = new Lokacija();
+            Odrediste = new Lokacija();
+            Komentar = new Komentar();
+            Id = brojac;
+            brojac++;
+        }
+
+        public int Id { get; set; }//ne prikazujemo
+        public DateTime DatumIVrijemePorudzbe { get; set; }
+        public Lokacija Lokacija { get; set; }
+        public TipoviAutomobila TipAutomobila { get; set; }
+        public String Musterija { get; set; }
+        public String Dispecer { get; set; }
+        public String Vozac { get; set; }
+        public Lokacija Odrediste { get; set; }
+        public Double Iznos { get; set; }
+        public Komentar Komentar { get; set; }
+        public StatusiVoznje StatusVoznje { get; set; }
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != GetType() || obj == null)
+            {
+                return false;
+            }
+            return Id == ((Voznja)obj).Id;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
